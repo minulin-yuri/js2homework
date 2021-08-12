427d365c469355bc21fcea317d71d8ea25924de8
minulin-yuri <minulin_yuri@mail.ru>
2021-08-12 22:19:43 +0300

    Добавил функцию поиска товара. Переделал все функции с использование ассинхронных запросов

--- js/app.js
+++ js/app.js
@@ -10,87 +10,145 @@ const app = new Vue({
             'cart': 'cart.html',
         },
         searchLine: '',
+        cartUrl: '/getBasket.json',
         catalogUrl: '/catalogData.json',
+        deleteUrl: '/deleteFromBasket.json',
+        addUrl: '/addToBasket.json',
         products: [],
         filteredProducts: [],
-        order: [],
+        cart: [],
+        orderAmount: '',
         imgCatalog: 'https://via.placeholder.com/200x150',
         imgCatalogMini: 'https://via.placeholder.com/50x50',
-        isVisibleCart: false,
-        notData: false,
+        isVisibleCart: false,//переменная для отображения корзины
+        notData: false,//переменная для проверки получения данных
     },
     methods: {
+        /**
+         * Функция получения данных в формате .json
+         * @param {*} url 
+         * @returns 
+         */
         getJson(url) {
             return fetch(url)
                 .then(result => result.json())
                 .catch(error => {
                     console.log(error);
-                    this.notData = true;
                 })
         },
+        /**
+         * Функция добавления товара в корзину
+         * (увеличения количества товара)
+         * @param {*} product 
+         */
         addProduct(product) {
-            if (this.order.length <= 0) {
-                this.order.push({
-                    'product': product,
-                    'count': 1,
-                });
-            } else {
-                let isIncludes = false;
-                this.order.forEach(item => {
-                    if (item.product.id_product == product.id_product) {
-                        item.count++;
-                        isIncludes = true;
+            this.getJson(`${API + this.addUrl}`)
+                .then(data => {
+                    if (data.result === 1) {
+                        let isIncludes = false;
+                        this.cart.forEach(item => {
+                            if (item.id_product === product.id_product) {
+                                item.quantity++;
+                                isIncludes = true;
+                            }
+                        });
+                        if (!isIncludes) {
+                            product.quantity = 1;
+                            this.cart.push(product);
+                        }
                     }
                 });
-                if (!isIncludes) {
-                    this.order.push({
-                        'product': product,
-                        'count': 1,
-                    });
-                }
-            }
         },
-        filterGoods() {
-            let regExp = new RegExp(this.searchLine, 'i');
-            this.filteredProducts = [];
-            this.products.forEach(item => {
-                if (regExp.test(item.product_name)) {
-                    this.filteredProducts.push(item);
-                }
-
-            })
+        /**
+         * Функиця уменьшения количества 
+         * (если количество 1, то удаление) товара из корзины
+         * @param {*} product 
+         */
+        deleteItem(product) {
+            this.getJson(`${API + this.deleteUrl}`)
+                .then(data => {
+                    if (data.result === 1) {
+                        (product.quantity > 1)
+                            ? product.quantity--
+                            : this.removeItem(product);
+                    }
+                });
         },
-        decrementCount(product) {
-            if (product.count === 1) {
-                this.order.pop(product);
-            } else {
-                product.count--;
-            }
-        },
+        /**
+         * Функция удаления товара из корзины
+         * @param {*} product 
+         */
         removeItem(product) {
-            this.order.pop(product);
+            this.getJson(`${API + this.deleteUrl}`)
+                .then(data => {
+                    if (data.result === 1) {
+                        this.cart.splice(this.cart.indexOf(product), 1);
+                    }
+                });
         },
+        /**
+         * Функция поиска товара
+         */
+        filterGoods() {
+            if (this.searchLine == '') {
+                this.getDefault();
+            }
+            let regexp = new RegExp(this.searchLine, 'i');
+            let rezult = [];
+            this.filteredProducts.forEach(item => {
+                if (regexp.test(item.product_name)) {
+                    rezult.push(item);
+                }
+            });
+            this.filteredProducts = rezult;
+        },
+        /**
+         * Функция дефолтного представления списка товаров
+         */
+        getDefault() {
+            this.filteredProducts = this.products;
+            this.searchLine = '';
+        }
+    },
+    computed: {
+
     },
     beforeCreated() {
 
     },
     created() {
-        this.getJson(`${API + this.catalogUrl}`)
-            .then(data => {
-                this.products = data;
-            });
+
     },
     beforeMount() {
 
     },
     mounted() {
-
+        //получение товаров корзины
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                this.orderAmount = data.amount;
+                for (let el of data.contents) {
+                    this.cart.push(el);
+                }
+            });
+        //получение списка товаров
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                    this.filteredProducts.push(el);
+                }
+            })
+            .catch(() => {
+                this.notData = true;
+                //при ошибке получения данных - заглушка "Нет данных"
+            });
     },
     beforeUpdate() {
 
     },
     updated() {
-        //this.deleteFilter();
+
     },
     beforeDestroy() {
 
--- js/main2.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-let productList = new ProductList();
-//console.log(productList.getSummaryPrice());
-
-
